4c30ef34736a60a55160bd23393a5bce9784a466
Eny Rueda <edrueda@4geeks.co>
2016-02-12 10:08:28 -0430

    vue-resource para el llamado ajax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,10 +26,13 @@ var data = {
             ]
         }
     ]
-}
+};
+
+//Vue.use(require('vue-resource'));
+Vue.http.headers.common['Authorization'] = 'Bearer eyJ1c2VyX2F1dGhlbnRpY2F0aW9uX2lkIjozNDA2Mn0:1aTvGC:bzdKugKkTvNisEcJ50lt5BCy7FI';
 
 var taigaProjectUrl = 'https://api.taiga.io/api/v1/milestones?project=107653';
-Vue.config.debug = true
+Vue.config.debug = true;
 new Vue({
     el: '#app',
     data: {
@@ -38,21 +41,15 @@ new Vue({
     },
     methods: {
         fetchData: function () {
-            var xhr = new XMLHttpRequest()
-            var self = this
-            xhr.open('GET', taigaProjectUrl)
-            xhr.setRequestHeader("Authorization", "Bearer eyJ1c2VyX2F1dGhlbnRpY2F0aW9uX2lkIjozNDA2Mn0:1aTvGC:bzdKugKkTvNisEcJ50lt5BCy7FI")
-
-            xhr.onload = function () {
-                self.projects = JSON.parse(xhr.responseText)
-            }
-            xhr.send()
+            this.$http.get(taigaProjectUrl).then(function(response) {
+                this.$set('milestones', response.data)
+            })
         },
         fetchData2: function () {
             for (var i=0; i<this.projects.length; i++) {
-                console.log(this.projects[i].user_stories)
+
             }
-        },
+        }
     },
     created : function() {
         this.fetchData()
@@ -60,60 +57,4 @@ new Vue({
     watch : {
         projects: 'fetchData2'
     }
-    /*created: function () {
-     this.fetchData()
-     }*/
-})
-
-// define the item component
-/*Vue.component('item', {
- template: '#item-template',
- props: {
- model: Object
- },
- data: {
- 'userhistories': this.methods.fetchData,
- },
- computed: {
- isFolder: function () {
- return this.model.children &&
- this.model.children.length
- }
- },
- methods: {
- fetchData: function () {
- var xhr = new XMLHttpRequest()
- var self = this
- xhr.open('GET', taigaProjectUrl)
- xhr.onload = function () {
- self.commits = JSON.parse(xhr.responseText)
- }
- xhr.send()
- },
- toggle: function () {
- if (this.isFolder) {
- this.open = !this.open
- }
- },
- changeType: function () {
- if (!this.isFolder) {
- Vue.set(this.model, 'children', [])
- this.addChild()
- this.open = true
- }
- },
- addChild: function () {
- this.model.children.push({
- name: 'new stuff'
- })
- }
- }
- })
-
- // boot up the demo
- var demo = new Vue({
- el: '#demo',
- data: {
- treeData: data
- }
- })*/
+});
